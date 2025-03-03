44279a2a7581d1bcfa6696f637248005dbe3b742
Sam Hart <sam@hxrts.com>
2025-03-03 11:00:13 +0800

    add scenario test with timeline, actor, and reasource interaction

--- test/Main.hs
+++ test/Main.hs
@@ -2,6 +2,10 @@ module Main where
 
 import qualified Test.Tasty as Tasty
 import qualified SimpleNetworkConfig
+import qualified TimelineScenarioTest
 
 main :: IO ()
-main = Tasty.defaultMain SimpleNetworkConfig.tests 
+main = Tasty.defaultMain $ Tasty.testGroup "Time Bandits Tests" 
+  [ SimpleNetworkConfig.tests
+  , TimelineScenarioTest.tests
+  ] 
